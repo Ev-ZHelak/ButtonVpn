0ed7b1f4c38811ff9a47f925d7575c92d0d94ded
Evgeniy_Zhelak <evgeniyzhelak@gmail.com>
2025-05-03 23:20:52 +0700

    Create vpn_toggle.sh

---
+++ vpn_toggle.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+VPN_NAME="StarGuard"  # Замените на имя вашего VPN
+
+# Проверяем статус VPN
+STATUS=$(scutil --nc list | grep "$VPN_NAME" | awk '{print $2}')
+
+if [ "$STATUS" = "(Connected)" ]; then
+    # Если VPN подключен - отключаем
+    scutil --nc stop "$VPN_NAME"
+    echo "VPN ($VPN_NAME) отключен."
+else
+    # Если VPN отключен - подключаем
+    scutil --nc start "$VPN_NAME"
+    echo "VPN ($VPN_NAME) подключен."
+fi
