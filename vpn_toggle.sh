cdfd6d476eae8fbe9ad5abbe36dc799fed5e9761
Evgeniy_Zhelak <evgeniyzhelak@gmail.com>
2025-05-05 23:21:01 +0700

    Update vpn_toggle.sh

--- vpn_toggle.sh
+++ vpn_toggle.sh
@@ -11,16 +11,20 @@ if [ "$STATUS" = "(Connected)" ]; then
         osascript -e "display notification \"VPN $VPN_NAME отключен\" with title \"Готово!\"" # Системное уведомление
         echo "VPN успешно отключён"
     else
-        osascript -e "display notification \"Ошибка при отключении VPN $VPN_NAME\" with title \"Ошибка!\"" 
+        osascript -e "display notification \"При отключении VPN $VPN_NAME\" with title \"Ошибка!\"" 
         echo "Ошибка при отключении VPN"
     fi
-else
+elif [ "$STATUS" = "(Disconnected)" ]; then
     # Если VPN отключен — подключаем
     if scutil --nc start "$VPN_NAME"; then
         osascript -e "display notification \"VPN $VPN_NAME подключен\" with title \"Готово!\""
         echo "VPN успешно подключён"
     else
-        osascript -e "display notification \"Ошибка при подключении VPN $VPN_NAME\" with title \"Ошибка!\"" 
+        osascript -e "display notification \"При подключении VPN $VPN_NAME\" with title \"Ошибка!\"" 
         echo "Ошибка при подключении VPN"
     fi
+else 
+    # Если VPN не существует
+    osascript -e "display notification \"VPN $VPN_NAME не существует\" with title \"Ошибка!\"" 
+    echo "Ошибка VPN $VPN_NAME не существует"
 fi
