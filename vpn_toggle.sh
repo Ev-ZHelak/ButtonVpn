99349a73472119b0f5e7ab032c34ec301b36798c
Evgeniy_Zhelak <evgeniyzhelak@gmail.com>
2025-05-05 23:03:58 +0700

    Update vpn_toggle.sh added notification

--- vpn_toggle.sh
+++ vpn_toggle.sh
@@ -6,11 +6,21 @@ VPN_NAME="ИмяVPN"  # Замените на имя вашего VPN
 STATUS=$(scutil --nc list | grep "$VPN_NAME" | awk '{print $2}')
 
 if [ "$STATUS" = "(Connected)" ]; then
-    # Если VPN подключен - отключаем
-    scutil --nc stop "$VPN_NAME"
-    echo "VPN ($VPN_NAME) отключен."
+    # Если VPN подключен — отключаем
+    if scutil --nc stop "$VPN_NAME"; then
+        osascript -e "display notification \"VPN $VPN_NAME отключен\" with title \"Готово!\"" # Системное уведомление
+        echo "VPN успешно отключён"
+    else
+        osascript -e "display notification \"Ошибка при отключении VPN $VPN_NAME\" with title \"Ошибка!\"" 
+        echo "Ошибка при отключении VPN"
+    fi
 else
-    # Если VPN отключен - подключаем
-    scutil --nc start "$VPN_NAME"
-    echo "VPN ($VPN_NAME) подключен."
+    # Если VPN отключен — подключаем
+    if scutil --nc start "$VPN_NAME"; then
+        osascript -e "display notification \"VPN $VPN_NAME подключен\" with title \"Готово!\""
+        echo "VPN успешно подключён"
+    else
+        osascript -e "display notification \"Ошибка при подключении VPN $VPN_NAME\" with title \"Ошибка!\"" 
+        echo "Ошибка при подключении VPN"
+    fi
 fi
